9d62ff28be9a046895f9b31346a78ad60c21aaa8
Ana-Louise <47090825+afranz100@users.noreply.github.com>
2019-05-06 17:13:54 -0400

    Add files via upload

---
+++ App.R
@@ -0,0 +1,17 @@
+library(shiny)
+library(dplyr)
+library(data.table)
+
+ui <- fluidPage(
+  titlePanel("Recidivism Data"),
+  dataTableOutput("mytable")
+)
+
+
+server <- function(input,output) {
+  output$mytable <- renderDataTable({
+    a <- recidivism[,1:8]
+  })
+}
+
+shinyApp(ui=ui,server=server)
